91b1bca36b51696d3fd1d0c210f7aceeacd82e6a
Eric Periou <eperiou@yahoo.com>
2017-08-01 03:17:17 -0500

    (feature) add project schema

---
+++ schemas.js
@@ -0,0 +1,35 @@
+artisans: {
+    username:
+    password:
+    email:
+    telephone:
+    address:{
+      streetaddress:
+      streetaddress2:
+      city:
+      state:
+      country:
+      county:
+      zip:
+    }
+    crafts: [craft1,craft2,craft3];
+    materials: []
+},
+
+purchasers : {
+  username:
+  password:
+  email:
+  following: [id,id,id,id]
+  purchasing: [id,id,id,id]
+}
+
+projects: {
+  creater_id:
+  buyer:
+  price:
+  category:
+  name:
+  description:
+  steps:[]
+}
