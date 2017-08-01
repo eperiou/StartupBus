626d2a7413126c60d3235b46db0a494cac771e8a
Eric Periou <eperiou@yahoo.com>
2017-08-01 15:11:11 -0500

    (feature) add $http request to profile component

--- client/app/profile/profile.component.js
+++ client/app/profile/profile.component.js
@@ -6,20 +6,24 @@ const uiRouter = require('angular-ui-router');
 import routes from './profile.routes';
 
 
-
 export class ProfileComponent {
-  'ngInject';
+  /*@ngInject*/
+  $http;
+  photos;
+  message;
+  int;
 
-  constructor() {
+  constructor($http) {
+    this.$http = $http;
     this.message = 'Hello';
     this.int = 1
     this.photos = [1, 2, 3, 4, 5];
   }
-  logConsole() {
-    this.int++;
-    console.log('yeahbuddy')
+  $onInit() {
+    this.$http.get('http://localhost:3000/api/projects')
+      .then((success)=>console.log(success,'success'))
+      .catch(err=>console.log(err,'error'))
   }
-
 }
 
 export default angular.module('startupBusApp.profile', [uiRouter])
